a5485f771839cb9e3a9951ce7b7890c08baf892a
amelie-thuerk <a.thuerk01@gmail.com>
2023-02-16 21:48:19 +0100

    positioned input and output box next to each other & displayed output using javascript

--- index.js
+++ index.js
@@ -1,62 +1,27 @@
-// "use strict"
+const choices = ["Rock", "Paper", "Scissor"]
+const output = document.getElementsByClassName("game-output")[0]
+const form = document.getElementById("game-form")
+form.addEventListener("submit", function (event) {
+    event.preventDefault()
 
-// let userCounter = 0
-// let comCounter = 0
+    let userChoice
+    const inputs = document.getElementsByName("option")
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            userChoice = inputs[i].value
+        }
+    }
 
-// // use a boolean or remove this line
-// let play = 1
+    if (userChoice === undefined) {
+        console.error("Select an input before submitting.")
+        return
+    }
 
-// while (play) {
-//     let userChoice = prompt("Rock, Paper or Scissor?")
-    
-//     if (userChoice === "q") {
-//         let userQuit = prompt("Do you really want to quit? Yes or No:")
-//         if (userQuit.toLowerCase() === "yes") {
-//             console.log("Exiting the game...")
-//             break
-//         }
-        
-//         if (userQuit.toLowerCase() === "no") {
-//             console.log("Continuing the game...")
-//             continue
-//         }
-//     }
-    
-//     const choices = ["rock", "paper", "scissor"]
-    
-//     if (!choices.includes(userChoice) || userChoice === "q") {
-//         console.log("Invalid option. Try again.")
-//         continue
-//     }
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)]
 
-//     console.log("you: " + userChoice);
+    // Check for winner and display user and computer score
 
-
-//     // instead of having the user type yes or no, use the confirm() function whhcih works similar to prompt
-//     let userQuit = ["q", "yes", "no", "Yes", "No"]
-    
-    
-//     let randomIndex = Math.floor(Math.random() * choices.length);
-//     let comChoice = choices[randomIndex]
-
-//     console.log("enemy: " + comChoice)
-    
-//     if (userChoice === comChoice) {
-//         console.log("draw")
-//     } else if (userChoice === "rock" && comChoice === "scissor") {
-//         console.log("you win")
-//         userCounter++
-//     } else if (userChoice === "paper" && comChoice === "rock") {
-//         console.log("you win")
-//         userCounter++
-//     } else if (userChoice === "scissor" && comChoice === "paper") {
-//         console.log("you win")
-//         userCounter++
-//     } else {
-//         console.log("you lose")
-//         comCounter++
-//     }
-    
-//     console.log("you - enemy\n" + " " + userCounter + "      " + comCounter)
-// }
-
+    output.innerHTML = `
+    <div>User: ${userChoice}</div>
+    <div>Computer: ${computerChoice}</div>`
+})
