3d950ce92ae5817343186622ad532f7400f3f544
amelie-thuerk <a.thuerk01@gmail.com>
2023-02-17 13:49:15 +0100

    results are now being displayed & added a counter for each player

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let userCounter = 0
+let comCounter = 0    
+
 const choices = ["Rock", "Paper", "Scissor"]
 const output = document.getElementsByClassName("game-output")[0]
 const form = document.getElementById("game-form")
@@ -18,10 +21,29 @@ form.addEventListener("submit", function (event) {
     }
 
     const computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    
+    if (userChoice === computerChoice) {
+        output.innerHTML = `<div class="result">draw</div>`
+    } else if (userChoice === inputs[0].value && computerChoice === inputs[2].value) {
+        output.innerHTML = `<div class="result"> you win</div>`
+        userCounter++
+    } else if (userChoice === inputs[1].value && computerChoice === inputs[0].value) {
+        output.innerHTML = `<div class="result">you win</div>`
+        userCounter++
+    } else if (userChoice === inputs[2].value && computerChoice === inputs[1].value) {
+        output.innerHTML = `<div class="result">you win</div>`
+        userCounter++
+    } else {
+        output.innerHTML = `<div class="result">you lose</div>`
+        comCounter++
+    }
+    
+    output.innerHTML += `
+        <div class="user">You</div>
+        <div class="com">Enemy</div>
+        <div class="user-choice">${userChoice}</div>
+        <div class="com-choice">${computerChoice}</div>`
 
-    // Check for winner and display user and computer score
-
-    output.innerHTML = `
-    <div>User: ${userChoice}</div>
-    <div>Computer: ${computerChoice}</div>`
-})
+    output.innerHTML += `
+        <div class="score">${userCounter} ${comCounter}</div>`
+})  
